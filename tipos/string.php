ba732f1b3a670767ac4fe6f0d3f20169b93137f2
Verônica Helena <desenvolveronica@gmail.com>
2022-07-24 09:58:08 -0300

    Tipo String

---
+++ tipos/string.php
@@ -0,0 +1,15 @@
+<div class="titulo"></div>
+
+<?php
+echo 'Eu sou uma string aspas simples <br>';
+echo "Eu sou aspas duplas <br>";
+var_dump("Aspas duplas");
+echo '<br>';
+var_dump('Aspas simples');
+echo '<br>';
+//concatenação
+
+echo 'Esta estring'. ' '.'se junta com esta outra';
+print('<br>Também existe a função PRINT usando parênteses');
+print'<br>Também existe a função PRINT sem parênteses';
+echo '<br><br>';
