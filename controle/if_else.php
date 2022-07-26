6fee3cffd4678a44dbe09abc97f1d69aed50bd0e
Verônica Helena <desenvolveronica@gmail.com>
2022-07-26 07:46:06 -0300

    Implementando ifelse

--- controle/if_else.php
+++ controle/if_else.php
@@ -6,3 +6,27 @@ if(true){
 }else{
     echo 'Impresso se falso <br>';
 }
+
+if(true) echo 'Sem o bloco <br>';
+
+if(false)
+    echo 'Se verdadeiro sem bloco <br>';
+    echo 'Se falso sem bloco e sem colocar ELSE<br>';
+
+if(false)
+    echo 'Mostra se for verdadeiro sem usar o bloco <br>';
+else 
+    echo 'Mostra se falso sem usar o bloco, mas colocando ELSE <br>';
+
+//else if 
+
+if(false){
+    echo 'Passo A <br>';
+}else if(false){
+    echo 'Caiu no Else If';
+}elseif(true){   //o else e if  podem ser escritos juntos ou separados
+    echo 'Chegando no segundo else if <br>';
+}
+else {
+    echo 'Último passo <br>';
+}
