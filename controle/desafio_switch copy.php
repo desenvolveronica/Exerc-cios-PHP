572f3b938a66822fe79fd17b94f9e4f55c804d8b
Verônica Helena <desenvolveronica@gmail.com>
2022-07-28 17:12:15 -0300

    Implemento

--- controle/desafio_switch copy.php
+++ controle/desafio_switch copy.php
@@ -18,6 +18,8 @@
         <option value="milha-Km" >Milha > Km</option>
         <option value="metro-Km" >Metro > Km</option>
         <option value="Km-metro" >Km > Metro</option>
+        <option value="celsius -fireraid" >celsius  > fireraid</option>
+        <option value="fireraid-celsius" >fireraid > celsius </option>
     </select>
     <button>Calcular</button>
 </f
@@ -29,6 +31,8 @@ echo '<br> <p class="divisao">Solução Vídeo</p> <hr>';
 const FATOR_KM_MILHA = 0.621371;
 const FATOR_METRO_KM = 1000;
 const FATOR_MILHA_KM = 0.621371;
+const FATOR_CELSIUS_FIRERAID = 9/5;
+const FATOR_FIRERAID_CELSIUS = 32;
 
 
 $param = $_POST['param'] ?? 0;
@@ -52,13 +56,24 @@ switch($_POST['conversao']){
         $distancia = $param / FATOR_METRO_KM;
         $mensagem = "$param Metros = $distancia Km ";
         break;
+    
+    case 'celsius -fireraid':
+        $distancia = $param * FATOR_CELSIUS_FIRERAID + 32;
+        $mensagem = "{$param}° Celsius = {$distancia}° Fireraid ";
+        break;
+    
+    case 'fireraid-celsius':
+        $distancia = ($param - FATOR_FIRERAID_CELSIUS) * 5/9 ;
+        $mensagem = "{$param}° Fireraid = {$distancia}° Celsius ";
+        break;
+        
 
     default: 
-        $mensagem = 'Sem valores';
+        $mensagem = "Nenhum valor calculado!";
 
 };
 
-echo $mensagem;
+echo "<p>$mensagem</p>";
 
 
 echo '<p class="divisao">Minha solução</p> <hr>';
@@ -72,26 +87,38 @@ $valorOptionResultado;
 switch($valorOption){
     case 'km-milha':
         $valorOptionResultado = $valorInput * 0.621371;
+        echo "{$valorInput} Km representam {$valorOptionResultado} Milhas <br>";
         break;
 
     case 'milha-Km': 
         $valorOptionResultado = $valorInput / 0.621371;
+        echo "{$valorInput} Milhas representam {$valorOptionResultado} Km <br>";
         break;
 
     case 'metro-Km':
         $valorOptionResultado = $valorInput / 1000;
+        echo "{$valorInput} Metros representam {$valorOptionResultado} Km <br>";
         break;
     
         case 'Km-metro':
             $valorOptionResultado = $valorInput * 1000;
+            echo "{$valorInput} Km representam {$valorOptionResultado} Metros <br>";
+            break;
+
+        case 'celsius -fireraid':
+            $valorOptionResultado = ($valorInput * 9/5) + 32 ;
+            echo "{$valorInput}° celsius representam {$valorOptionResultado}° fireraid <br>";
             break;
         
+        case 'fireraid-celsius':
+            $valorOptionResultado = ($valorInput - 32) * 5/9;
+            echo "{$valorInput}° fireraid representam {$valorOptionResultado}° celsius <br>";
+            break;
         default: 
             echo 'Sem valores <br>';
-
 }
 
-echo "{$valorInput} representa {$valorOptionResultado} de {$valorOption} <br>";
+
 
 echo '<p class="divisao">Restosta de SELECT é o VALUE<p>';
 
