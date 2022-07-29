53dfe7cf59a26d4c932ad7f9ce19cbd696593d2f
Verônica Helena <desenvolveronica@gmail.com>
2022-07-29 15:24:13 -0300

    Resolução desafio

--- array/desafio_sorteio.php
+++ array/desafio_sorteio.php
@@ -2,4 +2,20 @@
 
 <?php
 
-$nomes = ["Else", "Rapunzel", "Brance de Neve", "Cinderela"];
+$nomes = ["Elza", "Rapunzel", "Brance de Neve", "Cinderela"];
+print_r($nomes);
+echo '<br>';
+$resultado = array_rand($nomes); // array_rand pega o índice
+var_dump($resultado); //sendo feito o sorteio do índice
+echo '<br>';
+echo "<h1><center>A princesa escolhida foi : $nomes[$resultado]</center></h1> <br>";
+echo "<div center><h1>A princesa escolhida foi : $nomes[$resultado]</h1></div> <br>";
+
+?>
+<style>
+          [center]{
+                  color: purple;
+                  display: flex;
+                  justify-content: center;
+          }
+</style>
