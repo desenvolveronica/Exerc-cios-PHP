963dc468e607a632e8350cc1bb7043cb4256e77e
Verônica Helena <desenvolveronica@gmail.com>
2022-07-31 08:44:49 -0300

    Ajuste

--- array/get.php
+++ array/get.php
@@ -1,1 +1,24 @@
 <div class="titulo">$_GET</div>
+
+<?php
+echo '<p class="divisao">Para colocar informações em uma requisição tipo GET</p><hr><br>';
+echo '<p comentario>Passa os parâmetros da requisiçaõ</p>';
+echo '<br>';
+echo 'Coloca ? e ai você consegue passsar os parêmetros ex:nome=veronica';
+echo '<br>';
+echo 'Coloca o & para passar mais outro par/âmetro ex:nome=veronica&casa=b';
+echo '<br>';
+echo "{$_GET['nome']}"; //resposta: Array ( [nome] => diva )
+echo '<br>';
+print_r($_GET);
+echo '<br>';
+
+
+
+
+?>
+<style>
+          [comentario]{
+                    margin-top: -35px;
+          }
+</style>
