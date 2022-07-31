8a576d210ef68316a013fdef4f8aa1f4ed504885
Verônica Helena <desenvolveronica@gmail.com>
2022-07-31 09:51:59 -0300

    Requisição POST

--- array/post.php
+++ array/post.php
@@ -1,1 +1,42 @@
 <div class="titulo">$_POST</div>
+
+<form form action="#" method="post">
+        <input type="text" name="nome" placeholder="Nome">   <!-- Através do name que serve como chave para envio dos parâmetros -->
+        <input type="text" name="sobrenome" placeholder="Sobrenome">
+
+          <div selecao>
+          <select selecao name="estado" id="">
+                    <option value="AC">ACRE</option>
+                    <option value="BA">BAHIA</option>
+          </select>
+                    <button enviar>Enviar</button>
+          </div>
+</form>
+
+<?php
+print_r($_POST['nome']);
+echo ' ';
+print_r($_POST['sobrenome']);
+echo ' ';
+print_r($_POST['estado']);
+echo ' ';
+echo count($_POST);
+
+
+?>
+<style>
+          form > * {
+                    font-size: 1.3rem;
+          }
+          [enviar]{
+                    background-color: pink;
+                    
+          }
+          [selecao], button{
+                    font-size: 1.6rem;
+                    margin: 10px 20px
+          }
+
+
+
+</style>
