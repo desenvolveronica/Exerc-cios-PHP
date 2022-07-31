e2732779379ff614a222d9871e7815a8ca01a878
Verônica Helena <desenvolveronica@gmail.com>
2022-07-31 08:54:41 -0300

    POST

--- array/get.php
+++ array/get.php
@@ -4,9 +4,9 @@
 echo '<p class="divisao">Para colocar informações em uma requisição tipo GET</p><hr><br>';
 echo '<p comentario>Passa os parâmetros da requisiçaõ</p>';
 echo '<br>';
-echo 'Coloca ? e ai você consegue passsar os parêmetros ex:nome=veronica';
+echo '<p comentario1>Coloca ? e ai você consegue passsar os parêmetros ex:nome=veronica</p>';
 echo '<br>';
-echo 'Coloca o & para passar mais outro par/âmetro ex:nome=veronica&casa=b';
+echo '<p comentario1>Coloca o & para passar mais outro par/âmetro ex:nome=veronica&casa=b</p>';
 echo '<br>';
 echo "{$_GET['nome']}"; //resposta: Array ( [nome] => diva )
 echo '<br>';
@@ -20,5 +20,8 @@ echo '<br>';
 <style>
           [comentario]{
                     margin-top: -35px;
+          }
+          [comentario1]{
+                    margin-top: -60px
           }
 </style>
