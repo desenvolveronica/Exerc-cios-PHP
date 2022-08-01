ee77b8867483a7d0121c801ede5b08687c172117
Verônica Helena <desenvolveronica@gmail.com>
2022-08-01 16:30:39 -0300

    Segunda resposta

--- repeticoes/desafio_impressoes.php
+++ repeticoes/desafio_impressoes.php
@@ -15,3 +15,11 @@ for($indice = 0; $indice < $array[$indice]; $indice++){
           if($indice %2 === 1) {continue;}
           else {echo $array[$indice] . '<br>';}        
 };
+echo '<p>Outra forma de fazer (FOREACH)</p><hr>';
+
+foreach($array as $chave => $valor){
+          if($chave %2 !== 0) continue;
+          // echo" $array[$chave] <br>"; também funciona
+          echo "$valor  <br>";
+
+};
