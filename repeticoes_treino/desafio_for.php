01467afab3463ea75f16cf27e1d0a8664bc85f12
Verônica Helena <desenvolveronica@gmail.com>
2022-08-03 15:24:46 -0300

    resposta

--- repeticoes_treino/desafio_for.php
+++ repeticoes_treino/desafio_for.php
@@ -1,1 +1,16 @@
 <div class="titulo">Desafio for</div>
+
+<?php
+
+$array =['#', '##', '###', '####', '#####'];
+
+for($i = 0; $i < count($array); $i++){
+          echo "$array[$i] <br>";
+}
+echo '<br>';
+
+$impressao = '';
+for($cont = 1 ; $cont <= 5; $cont++){
+          $impressao .= '#';
+          echo $impressao .'<br>';
+}
