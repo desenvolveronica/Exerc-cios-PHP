430e4eb148740c185183f51562a26b23fd3a1637
Verônica Helena <desenvolveronica@gmail.com>
2022-08-05 10:11:45 -0300

    Sobre funções

--- funcoes/escopo.php
+++ funcoes/escopo.php
@@ -1,3 +1,20 @@
 <div class="titulo">Função & Escopo</div>
 
 <?php
+ function imprimirMensagem(){
+          echo "Olá ";
+          echo "Até a próxima <br>";
+ }
+
+ imprimirMensagem(); 
+ imprimirMensagem(); 
+ imprimirMensagem(); 
+
+ $variavel = 1; 
+echo "<br> Aqui fora o valor inicial da variável é {$variavel} <br>";
+ function trocaValor(){
+          $variavel = 2;
+          echo "Aqui dentro da função o valor da variável é {$variavel} <br>";
+ }
+ trocaValor();
+echo "Aqui fora da função o valor da variável volta a ser {$variavel}<br>";
