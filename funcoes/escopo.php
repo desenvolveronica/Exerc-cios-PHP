670ab56f02ef30ee03c30a449c055d17d1f4164e
Verônica Helena <desenvolveronica@gmail.com>
2022-08-05 10:19:59 -0300

    Variavel global

--- funcoes/escopo.php
+++ funcoes/escopo.php
@@ -18,3 +18,13 @@ echo "<br> Aqui fora o valor inicial da variável é {$variavel} <br>";
  }
  trocaValor();
 echo "Aqui fora da função o valor da variável volta a ser {$variavel}<br>";
+echo "<br><hr>";
+
+echo "Valor da variável até aqui é : {$variavel} <br>";
+function trocaValorDeVerdade(){
+          global $variavel;  //indicando que ela é global a mesma é declarada (neste caso conseguimos usar a variável fora do bloco)
+          $variavel = 10;
+          echo "O valor da variável dentro do bloco é : {$variavel} <br>";
+}
+trocaValorDeVerdade(); //neste ponto a função está sendo chamada
+echo "Mesmo fora da função o valor da variável é : {$variavel} / ISSO PQ FOI USADO O GLOBAL ANTES DE CHAMAR A VARIAVEL DENTRO DA FUNÇÃO <br>";
