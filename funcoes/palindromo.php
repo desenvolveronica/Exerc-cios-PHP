ae46ad7b080a84d100460bbdada9b3f2273fa1f7
Verônica Helena <desenvolveronica@gmail.com>
2022-08-05 16:16:57 -0300

    Segunda Resposta Palindromo

--- funcoes/palindromo.php
+++ funcoes/palindromo.php
@@ -8,8 +8,26 @@ $teste = 'etset';
 echo(strrev($teste))."<br>";
 echo $teste . "<br>";
 
+
 if(strrev($teste) ==="teste"){
           echo "Palindromo é igual <br>";
 }else{
           echo "Palindromo é diferente <br>";
 }
+echo "<hr>";
+
+function palindromo($palavra){
+           $ultimoIndice = strlen($palavra)-1;  //neste ponto está pegando o último índice da string => lembrando que ele começa no 0
+           for($i = 0; $i <= $ultimoIndice; $i++){
+                    if($palavra[$i] !== $palavra[$ultimoIndice - $i]){
+                              return "Não";
+                    }
+           }
+           return "Sim";
+}
+echo palindromo('arara') . "<br>";
+echo palindromo('bola') . "<br>";
+echo palindromo('teste') . "<br>";
+echo palindromo('oco') . "<br>";
+
+echo strlen('teste'); //strlen => 5 e ele tem 4 índices
