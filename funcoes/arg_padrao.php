ab32b542a08403586f0a610081659b88919a91aa
Verônica Helena <desenvolveronica@gmail.com>
2022-08-07 15:22:56 -0300

    Uso de argumento padrao

--- funcoes/arg_padrao.php
+++ funcoes/arg_padrao.php
@@ -1,1 +1,9 @@
 <div class="titulo">Argumento Padrão</div>
+
+<?php
+function saudacao($nome = "Senhor(a)", $sobrenome = "cliente"){
+          return "Seja bem vindo(a), $nome $sobrenome<br>";  // neste caso o argumento padrão vai aplicar um valor caso o parâmeto não seja passado na função
+}
+echo saudacao() . "<br>"; // exibe no echo os valores padões
+echo saudacao("Verônica", "Diva") . "<br>"; // exibe na tela os parâmetros recebidos
+echo saudacao(null, "Tona") . "<br>"; // Como o primeiro parâmetro é nulo NÃO vai aparecer nada para ele e toma para o segundo parâmetro
