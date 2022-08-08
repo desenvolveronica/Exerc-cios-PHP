749cef857deaefd1a7228d85721cc98974ea681e
Verônica Helena <desenvolveronica@gmail.com>
2022-08-08 11:30:06 -0300

    Observações e complemento

--- funcoes/anonimas.php
+++ funcoes/anonimas.php
@@ -1,1 +1,27 @@
 <div class="titulo">Funções Anônimas</div>
+
+<?php
+echo "Variável que recebe o NOME de uma função (em função anônima coloca a variável recebendo a function) <br>";
+$soma1 = function($a, $b){
+          echo "<span>Somando $a + $b = </span>";
+          return $a + $b;
+};
+$soma = function($a, $b){
+          echo "<span>Somando </span>";
+          return $a + $b;
+};  //neste caso precisa de colocar o (;) pois ela  é uma setença de código diferente de uma declaração de função
+
+echo $soma1(1,2) . "<br><br>";
+echo "Função recebendo outra como parâmetro <br><hr>";
+function executar($a, $b, $op, $function){
+          $resultado = $function($a, $b);
+          echo "$a $op $b = $resultado <br>";
+}
+
+executar(2, 3, '+', $soma);
+echo "<br> ";
+$multiplicacao = function($a, $b){
+          echo "$a * $b = ";
+          return $a * $b;
+};
+echo $multiplicacao(4, 5);
