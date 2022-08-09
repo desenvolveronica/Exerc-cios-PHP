10d858c81d8e4f9e15f6e4483b6edd8e8addd254
Verônica Helena <desenvolveronica@gmail.com>
2022-08-09 17:03:36 -0300

    complemento

--- funcoes/closure_callable.php
+++ funcoes/closure_callable.php
@@ -5,7 +5,17 @@ echo "Callable pode ser chamado <br>";
 $soma = function($a, $b){
           return $a + $b;
 };
-echo $soma(4,5) ."<br><br>";  //$soma é um Callable
-echo (is_callable($soma)? 'Sim, soma é um callable': 'Não'). "<br>";
-$soma = 1;
-echo (is_callable($soma)? 'Sim, soma é um callable': 'Não, desta forma soma não é um callable'). "<br>";
+echo $soma(4,5) ."<br>";  //$soma é um Callable
+echo (is_callable($soma)? 'Sim, soma é um callable': 'Não'). "<br><br>";
+
+
+$soma1 = 1;
+echo $soma1 ."<br>";
+echo (is_callable($soma1)? 'Sim, soma1 é um callable': 'Não, desta forma soma1 não é um callable'). "<br><br>";
+
+$soma2 = function($a, $b){
+          return $a + $b;
+};
+echo $soma2(2,3) ."<br>"; 
+echo (is_callable($soma2)? 'Sim, soma é um callable': 'Não, desta forma soma não é um callable'). "<br>";
+
