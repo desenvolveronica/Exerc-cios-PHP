6f71bb71696b881471d2d4e63861abd3f18b191d
Verônica Helena <desenvolveronica@gmail.com>
2022-08-09 18:41:25 -0300

    exemplo com fatorial

--- funcoes/fatorial_recursivo.php
+++ funcoes/fatorial_recursivo.php
@@ -1,3 +1,10 @@
 <div class="titulo">Fatorial recursivo</div>
-
-<?php
+<p>Resolvendo de forma recursiva (quebrando o problema inicial no mesmo problema porém, menores)</p>
+7! = 5040
+          =>7 * 6! (720 * 7 = 5040)
+                    =>6 * 5! (120 * 6 = 720)
+                              =>5 * 4! (5 * 24 = 120)
+                                        =>4 * 3! ( 4 * 6 = 24)
+                                                  =>3 * 2! (subistitui o fatorial de 2 por 2 => 2 * 3 = 6)
+                                                            =>2 * 1! (1! = 1 => 2 * 1 = 2)
+                                                                      => 1 condição de parada
