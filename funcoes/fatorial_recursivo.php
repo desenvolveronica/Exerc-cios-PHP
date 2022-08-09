9cb2be3eb70b68180207fd98ee5cb770db8065a3
Verônica Helena <desenvolveronica@gmail.com>
2022-08-09 18:48:14 -0300

    outro exemplo

--- funcoes/fatorial_recursivo.php
+++ funcoes/fatorial_recursivo.php
@@ -8,3 +8,20 @@
                                                   =>3 * 2! (subistitui o fatorial de 2 por 2 => 2 * 3 = 6)
                                                             =>2 * 1! (1! = 1 => 2 * 1 = 2)
                                                                       => 1 condição de parada
+
+
+<p>Outro problema resolvido de forma recusiva</p>
+obs: a representa até 
+4a1 
+          => 4+ 3a1
+                    => 3 + 2a1
+                              => 2 + 1a1
+                                        =>1 ponto de parada 
+
+*** resolvendo
+
+4a1 => 10
+          => 4+ 6
+                    => 3 + 3
+                              => 2 + 1
+                                        =>1
