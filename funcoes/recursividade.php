16f200a9c0b77e0993f1684dd12838fdbc98539f
Verônica Helena <desenvolveronica@gmail.com>
2022-08-10 15:34:53 -0300

    Primeiro exemplo

--- funcoes/recursividade.php
+++ funcoes/recursividade.php
@@ -1,1 +1,12 @@
 <div class="titulo">Recursividade</div>
+
+<?php
+
+function somaUmAte($numero){
+          $soma = 0;
+          for(; $numero >= 1; $numero--){  //neste caso não declara a varoável inicial visto que a mesma virá da chamadaq da function
+                    $soma += $numero;
+          }
+          return $soma;
+}
+echo somaUmAte(7);
