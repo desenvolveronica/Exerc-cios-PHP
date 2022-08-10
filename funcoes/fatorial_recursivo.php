500869725117128b23e9c6a88dd3ce8ff9c12606
Verônica Helena <desenvolveronica@gmail.com>
2022-08-10 16:45:48 -0300

    refetorando

--- funcoes/fatorial_recursivo.php
+++ funcoes/fatorial_recursivo.php
@@ -1,5 +1,6 @@
 <div class="titulo">Fatorial recursivo</div>
 <p>Resolvendo de forma recursiva (quebrando o problema inicial no mesmo problema porém, menores</p>
+<br>
 7! = 5040
           =>7 * 6! (720 * 7 = 5040)
                     =>6 * 5! (120 * 6 = 720)
@@ -25,3 +26,18 @@ obs: a representa até
                     => 3 + 3
                               => 2 + 1
                                         =>1
+
+
+
+
+<?php
+echo "<br><br>Refatorando<br>";
+
+function fatorial($numero){
+          //condição de parada
+          if($numero === 1){
+                    return 1;
+          } return $numero * fatorial($numero - 1); //cada passada onde é retirado 1 o $numero assume um outro valor
+}
+echo fatorial(7) . "<br>";
+
--- funcoes/recursividade.php
+++ funcoes/recursividade.php
@@ -13,7 +13,7 @@ echo somaUmAte(7) . "<br>";
 
 function outraSomaUmAte($numero){
           $soma = 0;
-          for($i = 1; $i <= $numero; $i++){  //neste caso não declara a varoável inicial visto que a mesma virá da chamadaq da function
+          for($i = 1; $i <= $numero; $i++){  //neste caso não declara a variável inicial visto que a mesma virá da chamada da function
                     $soma += $i;
           }
           return $soma;
