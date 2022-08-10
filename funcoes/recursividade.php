73bed4fc1a6bf5f81b1b6361894d35966b875b90
Verônica Helena <desenvolveronica@gmail.com>
2022-08-10 15:48:30 -0300

    outro exemplo

--- funcoes/recursividade.php
+++ funcoes/recursividade.php
@@ -9,4 +9,14 @@ function somaUmAte($numero){
           }
           return $soma;
 }
-echo somaUmAte(7);
+echo somaUmAte(7) . "<br>";
+
+function outraSomaUmAte($numero){
+          $soma = 0;
+          for($i = 1; $i <= $numero; $i++){  //neste caso não declara a varoável inicial visto que a mesma virá da chamadaq da function
+                    $soma += $i;
+          }
+          return $soma;
+}
+
+echo outraSomaUmAte(7) . "<br>";
