4f5a1b888520f4bf2e3d5855a53c2efbe0416585
Verônica Helena <desenvolveronica@gmail.com>
2022-08-11 17:58:21 -0300

    Solução usando recursividade

--- function/recursividade.php
+++ function/recursividade.php
@@ -11,10 +11,18 @@ echo "Exemplo 1 <br>";
 echo "**Soma de determinado número até 1 <br>";
 
 function soma($numero){
-          $valor = 0;
-          for($i = 0; $i <= $numero; $i++){
-                   $valor += $i; 
-                   
+          $valor = 0; //se não estartar esta variável ela dá erro de undefined
+          for($i = 0; $i <= $numero; $i ++){
+                    $valor += $i;
           }return $valor;
 }
+
 echo soma(7);
+echo "<hr><br>";
+
+function somaRecursiva($numero){
+          if($numero ===1)return 1;
+          return $numero + somaRecursiva($numero - 1);
+}
+echo "Solução usando recursividade";
+echo somaRecursiva(7);
