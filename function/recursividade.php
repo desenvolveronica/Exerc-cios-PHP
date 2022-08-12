f66cdc72c4c451dc230a069a69a98946fe841dcc
Verônica Helena <desenvolveronica@gmail.com>
2022-08-12 17:34:56 -0300

    Mais uma forma de fazer

--- function/recursividade.php
+++ function/recursividade.php
@@ -10,7 +10,7 @@ echo "<hr>";
 echo "Exemplo 1 <br>";
 echo "**Soma de determinado número até 1 <br>";
 
-function somaTodos($numero){
+function somaTodos($numero){ //decrementar um determinado número e somar todos eles até o ponto de parada que é ser = ao $numero
           $valor = 0; //se não estartar esta variável ela dá erro de undefined
           for($i = 0; $i <= $numero; $i ++){
                     $valor += $i;
@@ -40,4 +40,26 @@ function exibindo($a, $b, $op, $funcao){  //esta function  na verdade é uma var
           echo "<span>Resultado $a $op $b = $resultado</span>";
 }
 
-echo exibindo(3, 5, '+', $soma);
+echo exibindo(3, 5, '+', $soma) . "<br>";
+
+function todosAteUm($numero){
+                    $valor= 0;
+          for($i = 0; $i <= $numero; $i++ ){
+                    $valor += $i;
+          }return $valor;
+}
+
+echo todosAteUm(7) . "<br>";
+echo "<hr>";
+
+
+echo "Mais Uma Forma de Fazer <br>";
+function outraSoma($numero){
+          $valor = 0;
+          for($i = $numero; $i >= 1; $i --){  //neste caso não tem a variável com o valor de início pois o valor pode ser qualquer um
+                    $valor += $i;
+          }return $valor;
+}
+echo outraSoma(7);
+
+
