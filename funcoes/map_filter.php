30b11d2548d0fb5a7659a38eefee734a72be45a3
Verônica Helena <desenvolveronica@gmail.com>
2022-08-13 11:04:21 -0300

    Transformação com FORESCH

--- funcoes/map_filter.php
+++ funcoes/map_filter.php
@@ -1,3 +1,12 @@
 <div class="titulo">Map & Filter</div>
 
 <?php
+echo "Map gera um novo ARRAY do mesmo tamanho após aplicação de transformação(função) <br>";
+$notas = [5.8, 4.3, 3.2, 7.9];
+$notasFinais1 = [];
+
+echo "Conversão com foreach <br>";
+foreach ($notas as $nota){
+          $notasFinais1[] = round($nota) ;
+}
+print_r($notasFinais1);
