0fa62d89567a0c08e81596fa18c4ac48571aed2c
Verônica Helena <desenvolveronica@gmail.com>
2022-08-16 17:09:47 -0300

    Primeia resposta

--- classes_objetos/desafio_classe.php
+++ classes_objetos/desafio_classe.php
@@ -1,1 +1,16 @@
 <div class="titulo">Desafio Classe</div>
+
+<?php
+
+class Data {
+          public $dia;
+          public $mes;
+          public $ano; 
+
+          public function DiaMesAno($dia, $mes, $ano){
+                    echo  " $dia/$mes/$ano <br>";
+          }
+
+}
+$d1 = new Data();
+$d1 -> DiaMesAno(01,01, 1970);
