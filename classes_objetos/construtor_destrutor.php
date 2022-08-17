7c532177b1b0243d5ec4e7ce1edcd51262c02b80
Verônica Helena <desenvolveronica@gmail.com>
2022-08-17 11:07:55 -0300

    Primeira forma

--- classes_objetos/construtor_destrutor.php
+++ classes_objetos/construtor_destrutor.php
@@ -2,17 +2,18 @@
 
 <?php
 echo "Quando chamamos o NEW estamos CHAMANDO a função contruct <br>";
+echo "Primeira forma :<br>";
 class Pessoa {
           //ATRIBUTOS
           public $nome;
           public $idade;
 
-          function __construct($novoNome, $idade = 18) //indicar quais os parâmetros quer receber no momento que chamar o NEW
+          function __construct() //indicar quais os parâmetros quer receber no momento que chamar o NEW
           //podemos passar um valor PADRÃO para os parâmetros SE QUISER
           {
                  echo "Constructor invocado! <br>"; 
-                 $this->nome = $novoNome; //this apontando para $nome RECEBE o valor de $novoNome  
-                 $this->idade = $idade; //$this apontando parA $idade recebe o valor de $idade
+                 $this->nome = 'Diva'; //this apontando para $nome RECEBE o valor de $novoNome  
+                 $this->idade = 29; //$this apontando parA $idade recebe o valor de $idade
           }
 
           function __destruct() //ELE NÃO RECEBE NENHUM PARÂMETRO
@@ -21,14 +22,14 @@ class Pessoa {
           }
 
           public function apresentar(){
-                    return "$this->nome : $this->idade";
+                    return "Nome: $this->nome <br> Idade: $this->idade";
           }
 }
 
 echo " <br>THIS significa o atributo do OBJETO (estância da classe) <br>";
 echo "1- Estanciar a classe com NEW criando uma variável <br>";
-echo "2- Estanciar a classe <br>";
-echo "1- Estanciar a classe <br>";
+echo "2- Neste caso o constructor está SETANDO os valores e como ele aponta para this em tela aparece os valores do constructor <br><br>";
 
-$people = new Pessoa('Diva', 29);
+$people = new Pessoa();
 echo $people ->apresentar();     
+echo "<br>Segunda forma :<br>";
