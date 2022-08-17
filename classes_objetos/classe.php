175a3aa7da18e7a4c908beea917231825c7c6cd9
Verônica Helena <desenvolveronica@gmail.com>
2022-08-17 10:45:46 -0300

    Correção

--- classes_objetos/classe.php
+++ classes_objetos/classe.php
@@ -7,31 +7,32 @@ class Cliente {
          public $nome = 'Anônimo';  // public é um modificador de vizibilidade => neste caso e elestá vizivel FORA da CLASSE
          public $idade = 18; //nem preciso de colocar os valores
 
-         public function apresentar($nome, $idade){
-          echo "Olá me chamo $this->nome e tenho {$this->idade} anos."; //para acessar o valor que foi usado na criação usamos o THIS
+         public function apresentar(){ //neste ponto ($nome, $idade) eu obrigo que sempre seja setado um valor ao chamar a function
+          echo "Olá me chamo {$this->nome} e tenho {$this->idade} anos."; //para acessar o valor que foi usado na criação usamos o THIS
          }
 }
 
-$c1 = new Cliente(); //com o new foi feita uma estância(objeto) de classe Cliente
-$c1 -> apresentar('Verônica', 28);
+$c1 = new Cliente; //com o new foi feita uma estância(objeto) de classe Cliente
+$c1 -> apresentar();
 
-echo "<br><br> Para acessar o VALOR que foi usado na CRIAÇÃO da CLASSE é preciso usar o $THIS -> parâmetro <br>";
+echo "<br><br> Para acessar o VALOR que foi usado na CRIAÇÃO da CLASSE é preciso usar o $THIS -> parâmetro sem o $ <br>";
 echo "Para mudar o valor do atributo que está usando o THIS é preciso acessar o atributo e fazer uma nova ATRIBUIÇÃO <br>";
-$c1-> nome = 'Verônica';
-$c1 -> apresentar('Verônica', 28); //neste ponto a idade ainda está conforme foi criado no início
+$c1-> nome = 'Verônica'; //ele reconhece que é o nome da classe Cliente
+$c1 -> apresentar(); //neste ponto a idade ainda está conforme foi criado no início
 echo "<br><br>";
 $c1-> idade = 28;
-$c1 -> apresentar('Verônica', 28);
+$c1 -> apresentar();
 
 echo "<br>Criando outro objeto ( fazendo nova estância ) com NEW <br><hr>";
 
 $c2 = new Cliente; //com o NEW é feita uma nova estância
 $c2 -> nome = 'Diva';
-$c2 -> idade = 27;
+$c2 -> idade = 29;
 
-$c2-> apresentar('Verônica', 28);
+$c2-> apresentar();
 echo "<br>";
 
 echo "<br> Como foi inserido no método ($nome, $idade) para cada chamada ele pede para passar um valor para esses atributos <br>";
 echo "SOLUÇÃO: <br>";
-echo "Não passar nada na criação do MÉTODO ou seja:   public function apresentar(){}...";
+echo "Não passar nada na criação do MÉTODO ou seja:   public function apresentar(){}...<br>";
+echo "FEITO! <br>";
--- classes_objetos/construtor_destrutor.php
+++ classes_objetos/construtor_destrutor.php
@@ -1,1 +1,34 @@
 <div class="titulo">Construtor & Destrutor</div>
+
+<?php
+echo "Quando chamamos o NEW estamos CHAMANDO a função contruct <br>";
+class Pessoa {
+          //ATRIBUTOS
+          public $nome;
+          public $idade;
+
+          function __construct($novoNome, $idade = 18) //indicar quais os parâmetros quer receber no momento que chamar o NEW
+          //podemos passar um valor PADRÃO para os parâmetros SE QUISER
+          {
+                 echo "Constructor invocado! <br>"; 
+                 $this->nome = $novoNome; //this apontando para $nome RECEBE o valor de $novoNome  
+                 $this->idade = $idade; //$this apontando parA $idade recebe o valor de $idade
+          }
+
+          function __destruct() //ELE NÃO RECEBE NENHUM PARÂMETRO
+          {
+                    echo "Destructor CHAMADO! <br>";
+          }
+
+          public function apresentar(){
+                    return "$this->nome : $this->idade";
+          }
+}
+
+echo " <br>THIS significa o atributo do OBJETO (estância da classe) <br>";
+echo "1- Estanciar a classe com NEW criando uma variável <br>";
+echo "2- Estanciar a classe <br>";
+echo "1- Estanciar a classe <br>";
+
+$people = new Pessoa('Diva', 29);
+echo $people ->apresentar();     
