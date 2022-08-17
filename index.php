fd44f0722c1ccdadb35f393c91b32d686d788e0a
Verônica Helena <desenvolveronica@gmail.com>
2022-08-17 09:58:35 -0300

    Construtor & Destrutor

--- index.php
+++ index.php
@@ -20,6 +20,11 @@
                 <h3>8. Paradigma O.O</h3>
                 <ul>
                     <li>
+                        <a href="exercicio.php?dir=classes_objetos&file=construtor_destrutor">
+                            Construtor & Destrutor
+                        </a>
+                    </li>
+                    <li>
                         <a href="exercicio.php?dir=classes_objetos&file=classe">
                             Primeira Classe
                         </a>
