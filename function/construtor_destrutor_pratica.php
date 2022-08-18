771eac8f1ad70edfa37c3f8e94405e37c564b335
Verônica Helena <desenvolveronica@gmail.com>
2022-08-18 11:11:29 -0300

    Teste 3

--- function/construtor_destrutor_pratica.php
+++ function/construtor_destrutor_pratica.php
@@ -18,4 +18,33 @@ class Cliente{
 }
 
 $cliente = new Cliente();
-echo $cliente -> chamada();
+echo $cliente -> chamada(); //neste caso chama a function sem passar nada nos parâmetros pq está consumindo as informações da classe 
+//com o uso do $this->nome e $this->idade
+
+echo "<br> Novo teste <hr>";
+
+class Usuario {
+
+          public $nome;
+          public $idade;
+
+          public function chamada2($nome, $idade){
+                   return "Olá, meu nome é $nome e tenho $idade anos.";
+          }
+}
+$user = new Usuario;
+echo $user -> chamada2('Diva', 23) . "<br>";
+echo "<br> Novo teste <hr>";
+
+class Humano1 {
+          public $nome = 'Tuti'; //valor acessado quando é usado o $this->$nome
+          public $idade = 10;
+
+          public function chamada3($nome, $idade){
+                    return "Olá, meu nome é $nome e tenho $idade anos <br>";
+          }
+
+}
+
+$mano = new Humano1;
+echo $mano ->chamada3('Divona', 18);
