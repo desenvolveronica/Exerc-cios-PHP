8d72a213f4f3b50da798f7329fa44a0723986290
Verônica Helena <desenvolveronica@gmail.com>
2022-08-19 15:10:43 -0300

    Usando herança herdada da classe pai

--- classes_objetos/heranca.php
+++ classes_objetos/heranca.php
@@ -36,9 +36,35 @@ echo "**Herança com EXTENDS";
 echo "<br> ";
 echo "<br> ";
 echo "Quando passamos uma classe com EXTENDS de outra estamos usando o conceito de herança <br>";
-echo "Neste caso a nova classe herda atributos e métodos da classe mãe <br>";
+echo "Neste caso a nova classe herda atributos e métodos da classe mãe <br>     ";
+echo "<br>  Em suma a classe Pessoa13 que é a classe PAI ou seja,  uma SUPERCLASSE e é mais genérica <br>";
+echo "A classe Usuario que é a classe FILHA ou seja, é uma SUBCLASSE e mais ESPECÍFICA <br>";
+echo "Tradução:  A classe USUARIO tem TUDO que a classe PESSOA13 tem e terá outros elementos <br>";
+echo "<br>";
+
 
 
 class Usuario extends Pessoa13{
+          public $login;
 
+          function __construct($nome, $idade, $login)
+          {
+                   $this->nome = $nome;
+                   $this->idade = $idade;
+                   $this->login = $login; 
+                   echo "Usuário instanciado <br>";
+          }
+
+          public function apresentar(){
+                    echo "{$this->login}: {$this->nome}, {$this->idade} anos";
+          }
 }
+
+$usuario = new Usuario('Trafônica', 30, 'Nove@lesca');
+echo "Usuário tem acesso a function apresentar que está definida no escopo da sua classe <br>";
+$usuario -> apresentar(); //usuário tem acesso a função apresentar que está definida no escopo da sua classe.
+echo "<br>";
+echo "<br> ******ATENÇÃO <br>";
+echo "<br> Usuário tem acesso a função da classe PAI <br>";
+$usuario -> apresentarOutro('Trafônica' ,30); //nesta função pede nome e idade
+echo "<br><hr>";
