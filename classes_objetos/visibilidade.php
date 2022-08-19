8ec9a87c07ea13ef5cc034aa762439211b33c83c
Verônica Helena <desenvolveronica@gmail.com>
2022-08-19 16:08:48 -0300

    Acessando atributos com diferentes modificadores

--- classes_objetos/visibilidade.php
+++ classes_objetos/visibilidade.php
@@ -1,1 +1,27 @@
 <div class="titulo">Visibilidade</div>
+
+<?php
+echo "Trabalhando com modificadores de ACESSO <br>";
+
+class A {
+          public $publico = "Público";
+          protected $protegido = "Protegido";
+          private $private = "Privado";
+
+          public function mostrarA(){
+                    echo "a) Público: {$this->publico} <br>";
+                    echo "b) Protegido: {$this->protegido} <br>";
+                    echo "c) Privado: {$this->private} <br>";
+
+          }
+}
+
+echo "NOTAS: <br>  Uma função definida no escopo da classe tem acesso a TODOS os seu atributos. Mesmo os PRIVATE ";
+echo "<br>  Só a PRÓPRIA CLASSE consegue acessar um atributo PRIVATE <br>";
+echo "Só consegue acessar um atributo <strong>PUBLIC, PROTECTED</strong> ou <strong>PRIVATE</strong> usando o THIS <br>";
+echo "<br>";
+echo "<hr>";
+echo "<br>";
+
+$subA = new A;
+$subA -> mostrarA();
