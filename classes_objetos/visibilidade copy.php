6b67727a13da2bbcaf9945ceaeccc73d6f4b3034
Verônica Helena <desenvolveronica@gmail.com>
2022-08-20 16:04:16 -0300

    Complemento

--- classes_objetos/visibilidade copy.php
+++ classes_objetos/visibilidade copy.php
@@ -1,4 +1,34 @@
 <div class="titulo">Visibilidade</div>
 
 <?php
-echo "Trabalhando com modificadores de ACESSO <br>";
+echo "Trabalhando com modificadores de ACESSO ou seja, <strong> encapsulamento </strong> <br> ";
+echo "Para acessar as variáveis internas da classe sendo <strong>PUBLIC, PROTECTED </strong>e <strong>PRIVATE</strong>";
+echo " é preciso chamar o <strong>THIS -></strong> <br>";
+
+class A {
+          public $public = 'Público';
+          protected $protected = 'Protected';
+          private $private = 'Private';
+
+          public function mostraA(){
+
+                    $this->protegida();
+                    $this->privada();
+                    echo "Variável interna de A Pública : {$this->public} <br>";
+                    echo "Variável interna de A Protected : {$this->protected} <br>";
+                    echo "Variável interna de A Private : {$this->private} <br>";
+          }
+
+          protected function protegida(){
+                    echo "Esta é uma função PROTEGIDA <br>";
+          }
+
+          private function privada(){
+                    echo "Esta é uma função PRIVADA <br>";
+          }
+}
+
+$subA = new A;
+echo "<br>***Função interna da classe: mostraA()<br><br>";
+$subA -> mostraA();
+echo "<br>";
