9f2543769b11b2c1147e486e764b85346bbcae1a
Verônica Helena <desenvolveronica@gmail.com>
2022-08-22 11:34:03 -0300

    Membros estáticos new

---
+++ classes_objetos/static copy.php
@@ -0,0 +1,6 @@
+<div class="titulo">Membros Estáticos</div>
+
+<?php
+
+echo "<br>";
+echo "Cheguei aqui <br>";
--- index.php
+++ index.php
@@ -20,6 +20,11 @@
                 <h3>8. Paradigma O.O</h3>
                 <ul>
                     <li>
+                        <a href="exercicio.php?dir=classes_objetos&file=static copy">
+                            Membros Estáticos NEW
+                        </a>
+                    </li>
+                    <li>
                         <a href="exercicio.php?dir=classes_objetos&file=static">
                             Membros Estáticos
                         </a>
