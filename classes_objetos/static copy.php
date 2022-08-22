b4691ce0942ce0ac714fcc2848bc474fcfa03963
Verônica Helena <desenvolveronica@gmail.com>
2022-08-22 14:09:23 -0300

    Comentários

--- classes_objetos/static copy.php
+++ classes_objetos/static copy.php
@@ -1,6 +1,13 @@
 <div class="titulo">Membros Estáticos</div>
 
 <?php
+echo "***MEMBROS DE CLASSE <br>";
+echo "Podem ser <strong>DINÂMICOS</strong> ou <strong>ESTÁTICOS</strong>";
 
-echo "<br>";
+
+
+
+
+
+echo "<br><br>";
 echo "Cheguei aqui <br>";
