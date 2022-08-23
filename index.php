7e6c09e1ed9a26b2f75eb71147477330e05db8a8
Verônica Helena <desenvolveronica@gmail.com>
2022-08-23 09:41:15 -0300

    Interface

--- index.php
+++ index.php
@@ -20,6 +20,11 @@
                 <h3>8. Paradigma O.O</h3>
                 <ul>
                     <li>
+                        <a href="exercicio.php?dir=classes_objetos&file=interface">
+                            Interface
+                        </a>
+                    </li>
+                    <li>
                         <a href="exercicio.php?dir=classes_objetos&file=static copy">
                             Membros Estáticos NEW
                         </a>
