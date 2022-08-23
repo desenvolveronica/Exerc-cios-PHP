7ecb1848754fc850f5ab46e1655612d8a8828513
Verônica Helena <desenvolveronica@gmail.com>
2022-08-23 10:45:01 -0300

    Tipando retorno da function com :

--- classes_objetos/interface.php
+++ classes_objetos/interface.php
@@ -7,9 +7,29 @@ echo "Essas funções nao possuem corpo nem mesmo comportamento. <strong>SOMENTE
 echo "Esta função declarada na interface, recebe o <strong>comportamento na classe </strong> <br>";
 echo "A declaração de uma interface é similar a uma classe <br>";
 echo "<strong>***Uma INTERFACE é mais genérica que uma CLASSE***</strong><br>";
+echo "Uma CLASSE que implementa (implements) uma INTERFACE deve obrigatoriamente <strong>USAR O MÉTODO DA INTERFACE</strong> <br>";
 echo "<br><hr>";
 
 interface Animal {
           function respirar();
           //na interface a função NÃO tem corpo
 }
+
+interface Canino {
+          function agir() : string;
+}
+
+class Cachorro implements Animal, Canino {
+          function respirar(){
+                    echo "Respirar com oxigênio <br>";
+          }
+          function agir() : string{
+                    return  "Agir como canino <br>";
+          }
+}
+
+
+$dog = new Cachorro;
+$dog -> respirar();
+echo $dog -> agir();
+       
