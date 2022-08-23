12702be5bfc180c5a9a2381de84981ab949822e3
Verônica Helena <desenvolveronica@gmail.com>
2022-08-23 14:33:08 -0300

    Classe abstrata info

---
+++ classes_objetos/classe_abstrata.php
@@ -0,0 +1,16 @@
+<div class="titulo">Classe Abstrata</div>
+
+<?php
+echo "Uma classe abstrata (ABSTRACT) <strong>PODE TER </strong> métodos abstratos ou concretos<br>";
+echo "<strong>Uma classe concreta OBRIGATORIAMENTE todos os MÉTODOS são concretos</strong> <br>";
+echo "ABSTRACT é uma modificador de membros <br>";
+echo "UMA CLASSE ABSTRATA É UMA CLASSE INCOMPLETA";
+echo "<ul>
+<li>Interface: Tem apenas a assinatura do método</li>
+<li>Classe Abstratata: Pode ter ou não o corpo do método</li>
+<li>Classe Concerta: Obrigatoriamente tem o corpo do método</li>
+</ul>";
+echo "<hr>";
+echo "<ul><li>Métodos concretos possuem assinatura e corpo</li>
+<li>Métodos abstrato possuem assinatura APENAS</li>
+</ul>";
--- index.php
+++ index.php
@@ -20,6 +20,11 @@
                 <h3>8. Paradigma O.O</h3>
                 <ul>
                     <li>
+                        <a href="exercicio.php?dir=classes_objetos&file=classe_abstrata">
+                            Classe Abstrata
+                        </a>
+                    </li>
+                    <li>
                         <a href="exercicio.php?dir=classes_objetos&file=interface">
                             Interface
                         </a>
