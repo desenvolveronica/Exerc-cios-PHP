ec7196b0eccee0b467098adb216d4c8662aa637c
Verônica Helena <desenvolveronica@gmail.com>
2022-08-24 17:58:10 -0300

    Primeira tentativa

--- classes_objetos/desafio_erros.php
+++ classes_objetos/desafio_erros.php
@@ -1,3 +1,34 @@
 <div class="titulo">Desafio 7 Erros</div>
 
 <?php
+
+interface Template {
+          //interface recebe SOMENTE assinatura do método
+          function metodo1();
+          public function metodo2($parametro);
+
+}
+
+abstract class ClasseAbstrata implements Template {
+          //pode ter somente assinatura do método e pode ter corpo tambem
+          public function metodo3() {
+                    echo "Estou funcionando <br>";
+          }
+}
+
+class Classe extends ClasseAbstrata {
+          function __construct($parametro)
+          {
+               $this->parametro = $parametro;   
+               parent::metodo3();  
+          }
+          function metodo1(){
+                    echo "Método 1 <br>";
+          }
+          public function metodo2($parametro){
+                    echo "Método 2 <br>";
+          }
+}
+
+$exemplo = new Classe('olá'); 
+$exemplo-> metodo3();
