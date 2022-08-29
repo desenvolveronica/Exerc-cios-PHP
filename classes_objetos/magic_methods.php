6e99c9a2b8cfff016d40b19a80a0d16fb5dc59e3
Verônica Helena <desenvolveronica@gmail.com>
2022-08-29 07:55:56 -0300

    Métodos Mágicos

---
+++ classes_objetos/magic_methods.php
@@ -0,0 +1,3 @@
+<div class="titulo">Métodos Mágicos</div>
+
+<?php
--- index.php
+++ index.php
@@ -20,6 +20,11 @@
                 <h3>8. Paradigma O.O</h3>
                 <ul>
                     <li>
+                        <a href="exercicio.php?dir=classes_objetos&file=magic_methods">
+                            Métodos Mágicos
+                        </a>
+                    </li>
+                    <li>
                         <a href="exercicio.php?dir=classes_objetos&file=traits_02">
                             Traits #02
                         </a>
