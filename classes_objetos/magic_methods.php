baeb3f4323cfa870fbf7cf9abd6cfdad51b35c9c
Verônica Helena <desenvolveronica@gmail.com>
2022-08-29 10:35:01 -0300

    Método Call

--- classes_objetos/magic_methods.php
+++ classes_objetos/magic_methods.php
@@ -27,9 +27,21 @@ class Pessoinha {
                     echo $this . "<br>"; //this refere-se ao OBJETO e internamente chama o toString
           }
 
-          public function __get($atributs){
+          public function __get($atributs){ //esta função será chamada sempre que tentar acessar um atributo não definido
                     return "Lendo um atributo não declarado de valor: {$atributs} <br>"; //não posso usar o TIHS pois 
                     //o atributo não está no construtor
+          }
+
+          public function __set($atri, $valor){ //esta function será chamada sempre que houver tentativa 
+                    //de ALTERAR um atributo NÃODEFINIDO
+                    echo "Alterando atributo não declarado: {$atri} / {$valor} <br>";
+          }
+
+          public function __call($metodo, $params){ //ele é chamado sempre que chama um método no objeto que nao existe
+                    //necessário passar o método e os parâmetros
+                    echo "Tentando executar o método {$metodo} <br>";
+                    echo "com os parâmetros {$params} <br>";
+                    print_r($params);
           }
 
 }
@@ -58,9 +70,22 @@ echo "Não foi preciso chamar o get, mas como ele
 já estava definido é retornado o nome da variável não informada <br>";
 echo 'R: '. $pessoinha->nomeCompleto . '<br>';
 
-
 // echo Pessoinha::__get('outro'); gera erro pois o get precisa ser chamado pela instância (dinamicamente)
 
+echo "<hr>";
+echo "Acessando SET <br>";
+echo "Como o SET já está definido, ao tentar ALTERAR um atributo não declarado esta function será chamada <br>";
+$pessoinha->nomeCompleto = "Divona" ;
+echo "<br>";
+echo 'Usando o GET => ', $pessoinha->nomeCompleto;
+
+
+echo "<br><hr>";
+echo "Acessando CALL <br>";
+echo "Ele é chamado SEMPRE que chama no objeto um método que nao existe <br>";
+echo "O que é passado como parâmetro é um Array <br>";
+echo $pessoinha->funcNaoExiste();
+echo "<br>";
 
 
 echo "<br>Cheguei aqui <br>";     
