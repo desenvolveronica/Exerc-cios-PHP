a676d7c611992324b09d4efedf9148230723ab47
Verônica Helena <desenvolveronica@gmail.com>
2022-08-30 11:55:59 -0300

    Protegendo reedefinição de função

--- includes/include_arquivo.php
+++ includes/include_arquivo.php
@@ -5,7 +5,7 @@ echo "<p style='color:blue'>Carregando:  include_arquivo <br></p>";
 $variavel = "Estou definida em escopo global"; //variável definida diretamente dentro do arquivo tem escopo global
 
 
-if(!function_exists('soma')){
+if(!function_exists('soma')){  //forma de proteger a redefinição da função
           function soma($a, $b){
                     $result = $a + $b;
                     return "Soma de $a + $b = {$result} <br> ";
