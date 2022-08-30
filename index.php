2e3cd79e98a8f6f1d650cfaa23acb0ae245a4548
Verônica Helena <desenvolveronica@gmail.com>
2022-08-30 08:55:37 -0300

    Polimorfismo

--- index.php
+++ index.php
@@ -20,6 +20,11 @@
                 <h3>8. Paradigma O.O</h3>
                 <ul>
                     <li>
+                        <a href="exercicio.php?dir=classes_objetos&file=polimorfismo">
+                            Polimorfismo
+                        </a>
+                    </li>
+                    <li>
                         <a href="exercicio.php?dir=classes_objetos&file=magic_methods">
                             Métodos Mágicos
                         </a>
