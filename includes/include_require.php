3e756919a53991f070b622f9ea8e719134e9c343
Verônica Helena <desenvolveronica@gmail.com>
2022-08-31 11:47:19 -0300

    Arquivo inexistente passado com include

--- includes/include_require.php
+++ includes/include_require.php
@@ -1,1 +1,24 @@
 <div class="titulo">Include VS Require</div>
+
+<?php
+
+echo "<ul>
+<p>Include com arquivo inexistente</p>
+<li>Ao criar um INCLUDE com arquivo INEXISTENTE, a aplicação apenas gera um erro no console</li>
+<li>O código abaixo deste include vai ser interpretado normalmente</li>
+<li>Para vizualizar os erros use: ini_se('display_errors')</li>
+</ul>";
+
+echo "Include com arquivo inexistente <br>";
+include('arquivo_inexistente.php');
+
+
+echo "Echo após include com arquivo inexistente <br>";
+echo "Uso do display_errors que deve ser feito <strong>ANTES</strong> do INCLUDE com o arquivo errado<br>";
+ini_set('display_errors' , 1);
+include('arquivo_inexistente.php');
+
+echo "<br><hr>";
+
+
+
