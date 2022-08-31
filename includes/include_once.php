2f705f28844fb627a8802b3fed927639cba72f85
Verônica Helena <desenvolveronica@gmail.com>
2022-08-31 17:30:29 -0300

    Require ONCE

--- includes/include_once.php
+++ includes/include_once.php
@@ -1,1 +1,33 @@
 <div class="titulo">Include ONCE</div>
+
+<?php
+
+ini_set('display_errors', 1);  //pra mostar erros
+
+include('include_once_arquivo.php');
+
+//require('include_once_arquivo.php'); Fatal error: Cannot redeclare soma()
+//este erro pode ser barrado com function_exists()
+echo "Alterando o valor da variável 'variavelGlobal' <br>";
+$variavelGlobal= 'Valor alterado' . '<br>';
+echo $variavelGlobal;
+echo "<br><hr>";
+echo "Ao chamar o include do mesmo arquivo, no caso da FUNCTION estar protegida com o function_extists, 
+o valor da variável é REDEFINIDO <br><br>";
+
+echo "Include ONCE   => 'include_once'<br>";
+echo "Com a função ONCE para chamar 1x, ela não foi chamada novamente e o valor da variável continua o mesmo <br>";
+include_once('include_once_arquivo.php');
+echo $variavelGlobal;
+echo "<br>Lógica também funciona para o REQUIRE com 'require_once' <br>";
+require_once('include_once_arquivo.php');
+
+
+
+
+
+
+
+
+
+echo "<br><br>Cheguei aqui ";
--- includes/include_once_arquivo.php
+++ includes/include_once_arquivo.php
@@ -1,9 +1,20 @@
 <?php
 
-echo "Carregando: incude_once_arquivo ... <br>";
-$variavelGlobal = "Estou definida globalmente <br>";
+echo "Carregando: include_once_arquivo ... <br>";
+echo $variavelGlobal = "Estou definida globalmente <br>";
+
 
 function soma($a, $b){
           $resultado = $a + $b;
           return "A soma de $a e $b é = $resultado";
 }
+
+// if(!function_exists('soma')){
+//           function soma($a, $b){
+//                     $resultado = $a + $b;
+//                     return "A soma de $a e $b é = $resultado";
+//           }
+          
+// }else{
+//           echo "Function já foi passada";
+// }
