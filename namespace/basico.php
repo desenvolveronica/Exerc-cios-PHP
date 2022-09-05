64c401c28aab0b3da785cee7b7878b6eacf7eed8
Verônica Helena <desenvolveronica@gmail.com>
2022-09-05 17:00:51 -0300

    Acessar namespace

--- namespace/basico.php
+++ namespace/basico.php
@@ -1,1 +1,8 @@
+<?php namespace contexto; ?>
 <div class="titulo">Exemplo Básico</div>
+
+<?php
+echo "<p>O namespace é a primeira declaração em um arquivo PHP, mesmo antes do HTML</p>";
+
+echo "CONS para acessar namespace = '__NAMESPACE__ ' =>  " . __NAMESPACE__ . '<br>';
+
