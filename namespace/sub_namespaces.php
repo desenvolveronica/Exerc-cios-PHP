b13cf1ddee5068527986dc7758662c2131462949
Verônica Helena <desenvolveronica@gmail.com>
2022-09-12 09:11:20 -0300

    Ajustes

--- namespace/sub_namespaces.php
+++ namespace/sub_namespaces.php
@@ -4,10 +4,10 @@
 <?php
 echo __NAMESPACE__ . "<br>";
 echo "Criando contante com DEFINE <br>";
-define('App\const_Define', 'Const_Define');
-echo const_Define , "<br>";
+define('App\constante', 'criada no APP');
+echo 'Constante criada no NAMESPACE APP => ', constante , "<br>";  //VALOR :   criada no namespace APP
 echo "<br><hr>";
-echo "**Inserindo outro SUB-NAMESPACE <br><br>";
+echo "**Inserindo SUB-NAMESPACE <br><br>";
 
 namespace App\Principal; 
 //daqui pra baixo está no ESCOPO do novo SUB-NAMESPACE;
@@ -15,5 +15,12 @@ namespace App\Principal;
 echo __NAMESPACE__ . "<br>";
 echo "Variável está no contexto 'App\Principal\ nova' <br>";
 const nova = 123;
-echo nova;
+echo nova , '<br>';
+echo "<br><hr>";
+echo 'Constante criada no SUB-NAMESPACE <br>';
+echo "Esta constante tem o mesmo nome de outra, mas como estão em NAMESPACES diferentes ela não conflitam <br>";
+
+define('App\Principal\constante', 'Criada no SUB-NAMESPACE principal'); //VALOR:  criada no SUB-NAMESPACE principal
+echo '<br> R: ', constante . '<br>';
+
 
