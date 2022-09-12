ce8b085463eba9d7bf7bd01abdcf3e827e36df98
Verônica Helena <desenvolveronica@gmail.com>
2022-09-12 10:47:09 -0300

    Uso de Include_once

--- namespace/use_as.php
+++ namespace/use_as.php
@@ -1,3 +1,8 @@
-<div class="titulo">Use/As</div>
+<?php NAMESPACE Outro\Nome; ?>
 
+<div class="titulo">Use/As</div>
 <?php
+echo 'Estamos no: ' . __NAMESPACE__ . '<br>';
+include_once('./use_as_arquivo.php') ; //usando o include temos autonomia de usar os métodos e classes do outro arquivo
+
+
