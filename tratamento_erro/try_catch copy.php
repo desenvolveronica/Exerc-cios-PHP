a508af8b10e5672fdcc203309b308fd47e0ac151
Verônica Helena <desenvolveronica@gmail.com>
2022-09-14 15:20:37 -0300

    Treinamento

--- tratamento_erro/try_catch copy.php
+++ tratamento_erro/try_catch copy.php
@@ -2,3 +2,10 @@
 
 <?php
 
+echo "<p>Usamos quando temos uma sentença de código que PODE dar erro</p>";
+
+try{ //try é TENTAR
+          intdiv(7,0);
+} catch(Error $e){
+      echo "Gerou um erro <br>";    
+}
