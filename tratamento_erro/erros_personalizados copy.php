b6831a320b01aa62f1a4bf7724483f09ea337d06
Verônica Helena <desenvolveronica@gmail.com>
2022-09-20 10:29:15 -0300

    If para tratamento das situações

--- tratamento_erro/erros_personalizados copy.php
+++ tratamento_erro/erros_personalizados copy.php
@@ -5,11 +5,30 @@ echo "<ol>
 <p>Herança entre classes usamos o extends</p>
 <li>Exception é a classe base de todas as exceções</li>
 <li>Para uma exceção é passado parâmetros obrigatórios</li>
-<li></li>
+<li>'$ previous' é quando a exceção foi causada por uma outra exceção</li>
 </ol>";
 echo "<p style='color: blue'>O que determina que a classe é uma exceção é a herança onde usamos extends Exception</p>";
 class FaixaEtariaException extends Exception {
           function __construct($message, $code = 0, $previous = null){
-                    
+                    echo "Erro personalizado: $message <br>";
+                    parent::__construct($message, $code, $previous);
           }
+
 }
+$FaixaEtariaException = new FaixaEtariaException('Instanciando a classe');
+echo "<br>";
+function calcularTempoAposentadoria($idade){
+          if($idade < 18){
+                    throw new FaixaEtariaException('Ainda está muito longe');
+          }
+          if($idade > 70){
+                    throw new FaixaEtariaException('Já deveria estar aposentado');
+          }
+
+          return 70 - $idade;
+}
+
+$idadesAvaliadas = [15, 30, 60, 80];
+
+echo "Para tratar todas as idades é necessário um looping contendo um try e catch 
+<br> Se não houver o tratamento do erro no primeiro caso que nao chegar no momento do retorno já finalizaria o looping <br>";
