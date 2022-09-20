89df7f7daac488a3c5ae3780a6ba49d34636a556
Verônica Helena <desenvolveronica@gmail.com>
2022-09-20 09:57:19 -0300

    Complementos

--- tratamento_erro/erros_personalizados copy.php
+++ tratamento_erro/erros_personalizados copy.php
@@ -4,10 +4,12 @@
 echo "<ol>
 <p>Herança entre classes usamos o extends</p>
 <li>Exception é a classe base de todas as exceções</li>
-<li></li>
+<li>Para uma exceção é passado parâmetros obrigatórios</li>
 <li></li>
 </ol>";
-
+echo "<p style='color: blue'>O que determina que a classe é uma exceção é a herança onde usamos extends Exception</p>";
 class FaixaEtariaException extends Exception {
-
+          function __construct($message, $code = 0, $previous = null){
+                    
+          }
 }
