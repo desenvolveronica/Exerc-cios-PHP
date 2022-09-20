c1d82c0b5299767900244ae6a1e928bdee70f0a0
Verônica Helena <desenvolveronica@gmail.com>
2022-09-20 09:46:08 -0300

    Continuação

--- tratamento_erro/erros_personalizados copy.php
+++ tratamento_erro/erros_personalizados copy.php
@@ -3,7 +3,11 @@
 <?php
 echo "<ol>
 <p>Herança entre classes usamos o extends</p>
-<li></li>
+<li>Exception é a classe base de todas as exceções</li>
 <li></li>
 <li></li>
 </ol>";
+
+class FaixaEtariaException extends Exception {
+
+}
