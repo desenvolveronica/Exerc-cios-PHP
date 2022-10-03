7601c3cb6e28f8ac71be94a495800d8d728ce11d
Verônica Helena <desenvolveronica@gmail.com>
2022-10-03 16:15:14 -0300

    Funtion

--- tratamento_erro/gerenciador_erro.php
+++ tratamento_erro/gerenciador_erro.php
@@ -27,4 +27,22 @@ try {
           echo "Divisão por zero <br>";
 }
 echo "<br><hr>";
-echo "Cheguei aqui";
+error_reporting(E_ALL);
+include('arquivo_inexistente.php');
+echo "<br>";
+
+echo "stripos() => Localizar a posição da primeira ocorrência de uma string / não case sensitive <br>";
+
+//função responsável por filtrar as mensagens
+//se retornar TRUE ela filtra a mensagem 
+//se retornar FALSE a mensagem não será filtrada e SERÁ EXIBIDA
+function filtrarMensagem($errno, $errstring){
+          $text = 'include'; //texto que quero procurar
+          return !!stripos(" $errstring", $text); // em ' $errstring'=> está deixando a posição 0 vazia para que 
+          //o primeiro caracter comece no índice 1
+          //dupla negação para converter em booleano pois pode retornar um inteiro
+}
+echo "<p><strong style='color: red'>Alerta:</strong>  usando '' aspas simples o '!!stripos $errstring' não reconhece</p><br>";
+
+echo "<br><br>";
+echo "<br>Cheguei aqui";
