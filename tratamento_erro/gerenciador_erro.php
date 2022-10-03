c251f0048942cc44daddb2851685a7696bcf00c7
Verônica Helena <desenvolveronica@gmail.com>
2022-10-03 15:43:36 -0300

    Ajuste

--- tratamento_erro/gerenciador_erro.php
+++ tratamento_erro/gerenciador_erro.php
@@ -6,6 +6,7 @@
 <li>'ini_set('display_errors', 1)' : Exibir erros </li>
 <li>'error_reporting(E_ERROR)' : Define o nível de criticidade reportada</li>
 <li>'error_reporting(E_ALL)': Exibe todos os erros e advertências</li>
+<li>'error_reporting(~E_ALL)' Não exibe nada</li>
 </ul>
 <?php
 
@@ -13,11 +14,17 @@ ini_set('display_errors', 1);
 error_reporting(E_ERROR); //definindo nível de criticidade
 // echo 4 / 0 . "<br>";
 
+error_reporting(E_ALL); //definindo nível de criticidade para todos
+// echo 4 / 0 . "<br>";
+
+error_reporting(~E_ALL); //não exibe nenhum erro
+// echo 4 / 0 . "<br>";
+ 
+
 try {
            4 / 0 ;
 }catch(DivisionByZeroError $e){
           echo "Divisão por zero <br>";
 }
-
-
+echo "<br><hr>";
 echo "Cheguei aqui";
