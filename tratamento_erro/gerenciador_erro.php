88ca8826b20dbb051a1bc66a9fa859985af603c4
Verônica Helena <desenvolveronica@gmail.com>
2022-10-03 15:32:20 -0300

    Complemento

--- tratamento_erro/gerenciador_erro.php
+++ tratamento_erro/gerenciador_erro.php
@@ -1,3 +1,23 @@
 <div class="titulo">Error Handler</div>
 
-Error Handler => Gerenciador de erro do PHP 
+
+<ul>
+<p style="color: blue">Error Handler => Gerenciador de erro do PHP </p>
+<li>'ini_set('display_errors', 1)' : Exibir erros </li>
+<li>'error_reporting(E_ERROR)' : Define o nível de criticidade reportada</li>
+<li>'error_reporting(E_ALL)': Exibe todos os erros e advertências</li>
+</ul>
+<?php
+
+ini_set('display_errors', 1); 
+error_reporting(E_ERROR); //definindo nível de criticidade
+// echo 4 / 0 . "<br>";
+
+try {
+           4 / 0 ;
+}catch(DivisionByZeroError $e){
+          echo "Divisão por zero <br>";
+}
+
+
+echo "Cheguei aqui";
