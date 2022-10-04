31d6fe2bda20c98e208ab874047d4b490b5ddcb9
Verônica Helena <desenvolveronica@gmail.com>
2022-10-04 10:15:43 -0300

    Usando session

--- sessao/basico_sessao.php
+++ sessao/basico_sessao.php
@@ -1,1 +1,17 @@
 <div class="titulo">Sessão</div>
+<p style='color:blueviolet'> Para usar uma SESSÃO é necessário declarar no início do php</p> 
+<?php
+session_start();
+echo "<ul>
+<li>Temos um array associado ao SESSION</li>
+<li>Para acessar os elementos da session 'print_r($_session)' pois é um array</li>
+<li>O array '$_ session []' é composto de chaves</li>
+</ul>";
+
+echo print_r($_SESSION);
+echo "<br>";
+
+if(!$_SESSION['nome']){
+          $_session['nome'] = 'Diva';
+}
+
