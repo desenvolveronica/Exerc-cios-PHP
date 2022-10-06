08b8ee23190ed6678551b72c6ac1efb23f2d1b59
Veronica Helena <desenvolveronica@gmail.com>
2022-10-06 10:20:25 -0300

    Clonando sessão

--- sessao/gerenciando_sessao.php
+++ sessao/gerenciando_sessao.php
@@ -5,13 +5,24 @@ echo "Para trabalhar com sessão inicialmente precisamos usqar o 'session_start(
 echo "Recapitulando: Sessão é um array <br>";
 echo "Para acessar array usamos 'print_r(nome do arry)' <br>";
 echo "Para obeter o id da sessão usamos 'session_id()' <br>";
-session_start();
-echo session_id() . '<br>'; //641fb9d5e4dc21ccc27ab946d0f87ec2
+
+echo "<br> O 'session_id()' serve tanto para mostrar o id da sessão quando para passar o id que deseja acessar<br>";
+echo "Para isso passamos o ID dentro de STRING 'session_id('641fb9d5e4dc21ccc27ab946d0f87ec2')' <br>";
+session_id('c909afde707c8abe572ac2801b93937b');  //641fb9d5e4dc21ccc27ab946d0f87ec2
+echo "Para setar o 'session_id()' precisamos fazer ants do 'session_start()' <br>";
 echo "<br>";
+
+session_start();
+echo "<hr><br>";
+
+
 print_r($_SESSION);
 echo "<br>Em outro navegador o id muda pois a sessão mudou";
 echo "<br><br><hr>";
 echo "Usamos o '&' para acessar referência de memória <br>";
 
 $contador = &$_SESSION['contador'];
-echo $contador = $contador ? $contador +1 : 1; //parte inicial significa se o $contador estiver setado
+$contador = $contador ? $contador +1 : 1; //parte inicial significa se o $contador estiver setado
+echo $_SESSION['contador'];
+
+
