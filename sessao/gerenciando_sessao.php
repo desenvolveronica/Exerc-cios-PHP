b63d7c24ae1525817c9926d0703d34498fae572b
Veronica Helena <desenvolveronica@gmail.com>
2022-10-06 10:09:20 -0300

    Ajuste

--- sessao/gerenciando_sessao.php
+++ sessao/gerenciando_sessao.php
@@ -1,1 +1,17 @@
 <div class="titulo">Gerenciando Sessão</div>
+
+<?php
+echo "Para trabalhar com sessão inicialmente precisamos usqar o 'session_start()' <br>";
+echo "Recapitulando: Sessão é um array <br>";
+echo "Para acessar array usamos 'print_r(nome do arry)' <br>";
+echo "Para obeter o id da sessão usamos 'session_id()' <br>";
+session_start();
+echo session_id() . '<br>'; //641fb9d5e4dc21ccc27ab946d0f87ec2
+echo "<br>";
+print_r($_SESSION);
+echo "<br>Em outro navegador o id muda pois a sessão mudou";
+echo "<br><br><hr>";
+echo "Usamos o '&' para acessar referência de memória <br>";
+
+$contador = &$_SESSION['contador'];
+echo $contador = $contador ? $contador +1 : 1; //parte inicial significa se o $contador estiver setado
