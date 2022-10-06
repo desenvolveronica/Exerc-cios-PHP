0eb01347e3a821794f0f19235b1f83a591153272
Veronica Helena <desenvolveronica@gmail.com>
2022-10-06 11:19:28 -0300

    Limpando sessão

--- sessao/gerenciando_sessao.php
+++ sessao/gerenciando_sessao.php
@@ -1,29 +1,32 @@
 <div class="titulo">Gerenciando Sessão</div>
 
 <?php
-echo "Para trabalhar com sessão inicialmente precisamos usqar o 'session_start()' <br>";
+echo "<p style='color: blue'>Sessão</p>";
+echo "Para trabalhar com sessão inicialmente precisamos usar o 'session_start()' <br>";
 echo "Recapitulando: Sessão é um array <br>";
 echo "Para acessar array usamos 'print_r(nome do arry)' <br>";
 echo "Para obeter o id da sessão usamos 'session_id()' <br>";
 
+echo "<p style='color: green'>Sessão Compartilhada</p>";
 //sessão compartilhada
-echo "<br> O 'session_id()' serve tanto para mostrar o id da sessão quando para passar o id que deseja acessar<br>";
+echo "<br> A função 'session_id()' serve tanto para mostrar o id da sessão quando para passar o id que deseja acessar<br>";
 echo "Para isso passamos o ID dentro de STRING 'session_id('641fb9d5e4dc21ccc27ab946d0f87ec2')' <br>";
 session_id('c909afde707c8abe572ac2801b93937b');  // antes era: 641fb9d5e4dc21ccc27ab946d0f87ec2
 echo "Para setar o 'session_id()' precisamos fazer ants do 'session_start()' <br>";
 echo "<br>";
 
 session_start();
-echo "<hr><br>";
+echo "<hr>";
 
-
-// print_r($_SESSION);
-echo "Em outro navegador o id muda pois a sessão mudou";
-echo "<br><br><hr>";
-echo "Usamos o '&' para acessar referência de memória <br>";
+echo "<p style='color: green'>Observações</p>";
+echo "Acessando a session completa: ";
+print_r($_SESSION); //mostra toda a session
+echo "<br>Em outro navegador o id muda pois a sessão mudou";
+echo "<br>";
+echo "Usamos o '&' para acessar referência de memória <br> Neste ponto contador tem: ";
 
 $contador = &$_SESSION['contador'];
-$contador = $contador ? $contador +1 : 1; //parte inicial significa se o $contador estiver setado
+$contador = $contador ? $contador +1 :  1 ; //parte inicial significa se o $contador estiver setado
 echo $_SESSION['contador'] . "<br><br><hr>";
 
 
@@ -41,3 +44,14 @@ if($contador % 5 === 0){
 
 echo session_id() . "<br>"; //comando para mostar o ID
 
+echo "<p style='color: green'>Limpando a sessão</p>";
+echo "<ol>
+<li>Critério para limpar a sessão:</li>
+<li>Cade vez que o 'contador dor >= 15'  a sessão será destruida</li>
+<li>Vai limpar a  sessão</li>
+</ol>";
+
+if($contador >= 15){
+          session_destroy();
+          echo "Sessão destruida <br>";
+}
