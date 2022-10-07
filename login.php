06f1524d0ef67d470a794d63123444dcb304237c
Veronica Helena <desenvolveronica@gmail.com>
2022-10-07 18:06:57 -0300

    Ajustes

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head class="login">
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700" rel="stylesheet">
     <link rel="stylesheet" href="recursos/css/estilo.css">
@@ -14,7 +14,27 @@
 
     <main class="principal" style="margin-top: 45px">
         <div class="conteudo">
-           
+           <h3>Identifique-se</h3>
+
+           <?php  if($_SESSION['erros']): ?>
+                <div class="erros">
+                    <?php foreach($_SESSION['erros'] as $erro):  ?>
+                        <p><?= $erro ?></p>
+                    <?php endforeach ?>
+                </div>
+           <?php endif ?>
+
+           <form action="#" method="post">
+                <div>
+                    <label for="email">E-mail</label>
+                    <input type="email" name="email" id="email">
+                </div>
+                <div>
+                    <label for="senha">Senha</label>
+                    <input type="password" name="senha" id="senha">
+                </div>
+           </form>
+
         </div>
     </main>
     <footer class="rodape">
