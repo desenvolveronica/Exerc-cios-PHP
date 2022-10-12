13a484e697577dcdd0a5f9c7000d01cca3a4bca7
Veronica Helena <desenvolveronica@gmail.com>
2022-10-12 14:26:17 -0300

    Comentário treino tratamento de erros

--- tratamento_erro_treino/try_catch.php
+++ tratamento_erro_treino/try_catch.php
@@ -1,5 +1,6 @@
 <div class="titulo">Try & Catch</div>
 <p style="color: green">Usamos o try/catch quando temos um código parigoso que pode gerar erro</p>
+<p style="color: green; margin-top: -32px">Quando temos código assim, já colocamos dentro do try e tratamos se houver erro no catch</p>
 <?php
 
 try {
@@ -7,4 +8,23 @@ try {
 } catch (DivisionByZeroError $e) {
           echo "Você tentou dividir por 0 <br>";
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 echo "Cheguei aqui";
