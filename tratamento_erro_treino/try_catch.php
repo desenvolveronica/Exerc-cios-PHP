60653d685a7b0fc16bc4e1c0e7d95152fc9dcfb5
Veronica Helena <desenvolveronica@gmail.com>
2022-10-12 14:22:44 -0300

    Primeiro exemplo teste

--- tratamento_erro_treino/try_catch.php
+++ tratamento_erro_treino/try_catch.php
@@ -1,1 +1,10 @@
 <div class="titulo">Try & Catch</div>
+<p style="color: green">Usamos o try/catch quando temos um código parigoso que pode gerar erro</p>
+<?php
+
+try {
+          intdiv(7, 0);
+} catch (DivisionByZeroError $e) {
+          echo "Você tentou dividir por 0 <br>";
+}
+echo "Cheguei aqui";
